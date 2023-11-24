e3af28d612a2e3b2a6df96f2677f663b61370a31
sush01 <kandel.sushila242@gmail.com>
2023-11-24 14:46:56 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import{ ElementsModule} from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
+import { ModsModule } from './mods/mods.module';
+import { ViewsModule } from './views/views.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule,
+    ViewsModule,
+    ModsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
